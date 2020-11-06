c06515d6d0e77fe1e162a74c9119414aee648bf0
maybeyahweh <lebjr91@gmail.com>
2020-11-06 06:55:45 -0600

    updated some of the melee functionality, restricted some actions during other actions to prevent glitching

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -12,12 +12,15 @@ public class ThirdPersonMovement : MonoBehaviour
     public float gravity = -9.81f;
     public float jumpHeight = 3;
     Vector3 velocity;
+    bool isRolling = false;
     bool isGrounded;
     bool isEquipped = false;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
-
+    bool isBlocking = false;
+    bool isAttacking = false;
+    bool isRunning = false;
     float turnSmoothVelocity;
     public float turnSmoothTime = 0.1f;
 
@@ -32,36 +35,69 @@ public class ThirdPersonMovement : MonoBehaviour
             velocity.y = -2f;
         }
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded && isBlocking == false && isAttacking == false)
         {
             anim.SetTrigger("Jump");
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
 
-        if (Input.GetButtonDown("Fire3") && isGrounded)
+        if (Input.GetButtonDown("Fire3") && isGrounded && isBlocking == false)
         {
+            isRunning = true;
             speed = 8;
         }
 
-        if(Input.GetButtonUp("Fire3") && isGrounded)
+        if(Input.GetButtonUp("Fire3") && isGrounded && isBlocking == false)
         {
+            isRunning = false;
             speed = 4;
         }
-
-        if (Input.GetKeyDown(KeyCode.Tab))
+        if (Input.GetKeyDown(KeyCode.Alpha1) && isEquipped == true)
         {
-            isEquipped = !isEquipped;
+            
+            anim.SetTrigger("Unequip");
+            isEquipped = false;
             anim.SetBool("Equipped", isEquipped);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && isEquipped == false)
+        {
+            isEquipped = true;
             anim.SetTrigger("Equip");
+            anim.SetBool("Equipped", isEquipped);
         }
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            isAttacking = true;
             speed = 0;
             anim.SetTrigger("Attack");
+            StartCoroutine(WaitForAttack());
         }
 
-        if (Input.GetKeyUp(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            isBlocking = true;
+            anim.SetBool("isBlocking", true);
+            anim.SetTrigger("Block");
+            speed = 0;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            speed = 4;
+            isBlocking = false;
+            anim.SetBool("isBlocking", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            speed = 12;
+            anim.SetTrigger("Dodge");
+            WaitForDodge();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0) && isBlocking == false)
         {
             speed = 4;
         }
@@ -92,6 +128,22 @@ public class ThirdPersonMovement : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        IEnumerator WaitForAttack()
+        {
+            yield return new WaitForSecondsRealtime(6);
+            isAttacking = false;
+            speed = 4;
+        }
+
+        IEnumerator WaitForDodge()
+        {
+            yield return new WaitForSeconds(10f);
+            if (isRunning == true)
+                speed = 8;
+            else
+                speed = 4;
         }
     }
 }
